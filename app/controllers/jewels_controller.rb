b1647f20853d0cc507c4bd3f614e8d501a4c290e
Eliza Dunaway & Nathan Luo <pair+elizaway.nathanluo112@devbootcamp.com>
2015-11-11 15:13:10 -0500

    Adds refactored solution

--- app/controllers/jewels_controller.rb
+++ app/controllers/jewels_controller.rb
@@ -17,3 +17,20 @@ post '/jewels' do
   redirect('/')
 end
 
+get '/jewels/:id/edit' do
+  @jewel = Jewel.find(params[:id])
+  erb :'edit'
+end
+
+put '/jewels/:id' do
+  @jewel = Jewel.find(params[:id])
+  # puts(params)
+  hsh={
+    mineral: params[:mineral],
+    facets: params[:facets],
+    location: params[:location]
+  }
+  @jewel.update_attributes(1)
+  redirect("/jewels/#{@jewel.id}")
+end
+
